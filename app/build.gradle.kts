f5cc72bef5e73abafc96e0ac9445d5b386d7e030
greyhoundshop073-coder <greyhoundshop073@gmail.com>
2026-08-31 22:50:37 +0100

    Add Compose material icons for FYNX navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-core")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
