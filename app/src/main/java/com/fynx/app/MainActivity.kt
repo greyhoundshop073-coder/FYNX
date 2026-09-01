958acf9cbb45c9fbfc2a9873605fcb8488eeb56b
greyhoundshop073-coder <greyhoundshop073@gmail.com>
2026-09-01 11:24:37 +0100

    Initialize FYNX notification channels safely

--- app/src/main/java/com/fynx/app/MainActivity.kt
+++ app/src/main/java/com/fynx/app/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.fynx.app.ui.FynxApp
+import com.fynx.app.ui.FynxNotificationFoundation
 import com.fynx.app.ui.FynxTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FynxNotificationFoundation.createChannels(this)
         setContent { FynxTheme { FynxApp() } }
     }
 }
