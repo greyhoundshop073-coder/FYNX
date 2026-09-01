daa0d2152819403a080cc949242b3b14edf5ff29
greyhoundshop073-coder <greyhoundshop073@gmail.com>
2026-09-01 15:45:43 +0100

    Add Compose extended Material icons required by FYNX navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("io.github.webrtc-sdk:android:144.7559.14")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
