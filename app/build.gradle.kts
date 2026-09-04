239dca50bfcd24b7bab09167f19d33fd306be538
greyhoundshop073-coder <greyhoundshop073@gmail.com>
2026-09-04 21:07:21 +0100

    Fix RED build by restoring compatible Kotlin JVM configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,9 +21,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-    }
+    kotlinOptions { jvmTarget = "17" }
     buildFeatures { compose = true }
 }
 
